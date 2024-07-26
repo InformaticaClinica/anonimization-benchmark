6f2958cdeb62265ed727596c081fc734ad0478fa
Petter Axcell <petteraxcell03@gmail.com>
2024-07-26 13:44:35 +0200

    all works

--- llm/prompt_handling/haiku_prompt_handler.py
+++ llm/prompt_handling/haiku_prompt_handler.py
@@ -2,9 +2,14 @@ from .prompt_handler import PromptHandler
 
 class HaikuPromptHandler(PromptHandler):
     def format_prompt(self, data: dict):
-        return f"""\n\nHuman:[INST]{data["system"]}
-
-
-                {data["user"]}
-            
-            \n\nAssistant:"""
+        return [data["system"], [
+                {
+                    "role": "user",
+                    "content": [
+                    {
+                        "type": "text",
+                        "text": f"""{data["user"]}"""
+                    }
+                    ]
+                }
+            ]]
--- llm/strategy/haiku3_model.py
+++ llm/strategy/haiku3_model.py
@@ -6,19 +6,18 @@ from llm.prompt_handling.haiku_prompt_handler import HaikuPromptHandler
 class Haiku3Model(LLMStrategy):
     def __init__(self):
         super().__init__()
+        self._client = boto3.client('bedrock-runtime', region_name='eu-west-2')
         self._model_id = "anthropic.claude-3-haiku-20240307-v1:0"
-        print(self._model_id)
-        self._client = boto3.client('bedrock-runtime', region_name='eu-west-3')
         self._prompt_handler = HaikuPromptHandler()
         self._top_k = 50
-        self._accept = 'application/json'
-        self._contentType = 'application/json'
 
 
-    def create_body(self, prompt) -> str:
+    def create_body(self, prompt, system_prompt) -> str:
         return json.dumps({
-            "prompt": prompt,
-            "max_tokens_to_sample":self._max_gen_len,
+            "anthropic_version": "bedrock-2023-05-31",
+            "system": system_prompt,
+            "messages": prompt,
+            "max_tokens":self._max_gen_len,
             "temperature":self._temperature,
             "top_p":self._top_p,
             "top_k":self._top_k
@@ -28,13 +27,12 @@ class Haiku3Model(LLMStrategy):
         response = self._client.invoke_model(
             modelId=self._model_id,
             body=body,
-            accept=self._accept,
-            contentType=self._contentType
         )
         response = json.loads(response.get('body').read())
-        return response["completion"]
+        return response["content"][0]["text"]
+
 
     def generate_prompt(self, model_prompt: dict) -> str:
-        prompt = self._prompt_handler.format_prompt(model_prompt)
-        body = self.create_body(prompt)
+        [system_prompt, prompt] = self._prompt_handler.format_prompt(model_prompt)
+        body = self.create_body(prompt, system_prompt)
         return self.invoke_model(body)
