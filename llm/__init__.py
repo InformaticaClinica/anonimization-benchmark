dc81f58a87f78effda377eeec4f6b9c3b7a13181
Petter Axcell <petteraxcell03@gmail.com>
2024-09-03 09:33:51 +0200

    reestructure

---
+++ llm/__init__.py
@@ -0,0 +1,21 @@
+from .context.llm_context import LLMContext
+from .strategy.big_llama_model import BigLlamaModel
+from .strategy.big_llama3_1_model import BigLlama3_1Model
+from .strategy.small_llama_model import SmallLlamaModel
+from .strategy.big_mistral_model import BigMistralModel
+from .strategy.haiku3_model import Haiku3Model
+from .strategy.sonet3_model import Sonet3Model
+from .strategy.opus3_model import OpusModel
+from .strategy.sonet3_5_model import Sonet3_5Model
+
+__all__ = [
+    "LLMContext",
+    "BigLlamaModel",
+    "BigLlama3_1Model",
+    "SmallLlamaModel",
+    "BigMistralModel",
+    "Haiku3Model",
+    "Sonet3Model",
+    "OpusModel",
+    "Sonet3_5Model"
+]
--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 # MODELS
-from llm.context.llm_context import LLMContext
-from llm.strategy.big_llama_model import BigLlamaModel
-from llm.strategy.big_llama3_1_model import BigLlama3_1Model
-from llm.strategy.small_llama_model import SmallLlamaModel
-from llm.strategy.big_mistral_model import BigMistralModel
-from llm.strategy.haiku3_model import Haiku3Model
-from llm.strategy.sonet3_model import Sonet3Model
-from llm.strategy.opus3_model import OpusModel
-from llm.strategy.sonet3_5_model import Sonet3_5Model
-#from llm.strategy.chatgpt_model import ChatGPTModel
+from llm import LLMContext, BigLlamaModel, BigLlama3_1Model, SmallLlamaModel, BigMistralModel, Haiku3Model, Sonet3Model, OpusModel, Sonet3_5Model
 
 import os
-
 
 
 def anonimized_loop(llm, name_model, data):
@@ -24,8 +14,6 @@ def anonimized_loop(llm, name_model, data):
 
 def main():
     print("Hello world")
-
-
 
 
 if __name__ == "__main__":
